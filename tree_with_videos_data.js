e19021b40f3ff11d9caa1f4fd4a9c977a9d00066
atelierdechile <atelierdechile@users.noreply.github.com>
2017-05-24 18:49:57 -0400

    Create tree_with_videos_data.js

---
+++ tree_with_videos_data.js
@@ -0,0 +1,49 @@
+var treeData = [
+  {
+    "name": "MentesBrillantes",
+    "parent": "null",
+    "children": [
+      {
+            "name": "Temporada 1"
+            ,"_children": [
+               {  
+                "name":   "Mentes Brillantes 1 - Pablo Valenzuela",
+                 "url": "https://www.youtube.com/v/ZmzUksziaCg"    } ,
+              {  
+                "name":   "Mentes Brillantes 1 - Ricardo Baeza",
+                 "url": "https://www.youtube.com/v/36BXjU7qvPQ"
+              },
+              {  
+                "name":   "Mentes Brillantes 1 - Carlos Conca",
+                 "url": "https://www.youtube.com/v/nRPOUSD1GU8"
+              },         
+              {
+              "name":    "Mentes Brillantes 1 - Ramón Latorre"
+              ,"url":   "https://www.youtube.com/v/voogNkB14gM"
+              }
+]
+},
+{
+            "name": "Temporada 2"
+            ,"_children": [
+               {  
+                "name":   "Mentes Brillantes 2 - Jorge Allende",
+                 "url": "https://www.youtube.com/v/joXU0YgKu4Q"
+              },
+              {  
+                "name":   "Achieve List Growth Results",
+                 "url": "https://www.youtube.com/v/64ERoj3wpxw"
+              },
+
+    { "name":    "General Electric Customer Success"
+       , "url":  "https://www.youtube.com/v/nHaBCcn-9BU"
+    },
+
+     {"name":    "News Corp Australia Customer Success"
+       ,"url":   "https://www.youtube.com/v/I3aefSZtd8w"
+    }
+]
+}
+]
+  }
+];
